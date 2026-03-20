4efbab02a69a0a03479fea0c9f8329623cc8aa15
Steve Simpson <steve@grafana.com>
2026-03-20 12:14:58 +0100

    Alerting: Return rule title from historian notification counting API.
    Returns the rule title from groupLabels['alertname'] if it exists, to save the
    client having to lookup the rule name all the time. Note that it will still be
    necessary to lookup the rule title if alertname is not used for grouping, but
    this avoids the lookup in the common case.

--- apps/alerting/historian/pkg/apis/alertinghistorian_manifest.go
+++ apps/alerting/historian/pkg/apis/alertinghistorian_manifest.go
@@ -451,6 +451,12 @@ var appManifestData = app.ManifestData{
 										Type: []string{"string"},
 									},
 								},
+								"ruleTitle": {
+									SchemaProps: spec.SchemaProps{
+										Type:        []string{"string"},
+										Description: "RuleTitle is returned if grouping by ruleUID.",
+									},
+								},
 								"ruleUID": {
 									SchemaProps: spec.SchemaProps{
 										Type: []string{"string"},
